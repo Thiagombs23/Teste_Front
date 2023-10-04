9ed231bf9464a5d4a114341bcf1f290c8ed8ff05
Fatec <jcr@fatec.sp.gov.br>
2023-10-03 22:28:04 -0300

    Atualização Grid_main

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,36 +1,39 @@
 import React from "react";
 import MainNavbar from "../Components/Navbar/Navbar";
-
-import Header from "../Components/Home/Header"; // Import the Header component
-
-
+import Header from "../Components/Home/Header";
 import CardBike from "../Components/Home/CardBike";
 import Search from "../Components/Home/Search";
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Home = () => {
+  const customColStyle = {
+    marginBottom: "10px", // Reduzimos a margem inferior em 10px
+  };
+
+  const cardStyle = {
+    marginTop: "15px", // Adicionamos uma margem superior para centralizar verticalmente
+    marginBottom: "15px", // Adicionamos uma margem inferior para centralizar verticalmente
+  };
+
   return (
-    <div>
+    <div style={{ minHeight: "100vh", backgroundColor: "#f0f2f5" }}>
       <MainNavbar />
-      
+
+      <Container fluid>
         <Header />
-
-        <Search/>
-
-      
-     
-        <div style={{display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap", alignContent:"center", justifyContent: "center"}}>
-        <CardBike/>
-        <CardBike/>
-        <CardBike/>
-        <CardBike/>
-        <CardBike/>
-        <CardBike/>
-        <CardBike/>
-        </div>
-
+        <Search />
+        <Row className="justify-content-center">
+          {[...Array(8)].map((_, index) => (
+            <Col key={index} xs={12} sm={6} md={4} lg={3} style={customColStyle}>
+              <div style={cardStyle}> {/* Envolver CardBike com uma div */}
+                <CardBike />
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
